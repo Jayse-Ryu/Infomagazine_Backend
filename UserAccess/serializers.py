94ad38bbe57be60cbadf3029ad23ddd8fe2f57e3
jayseryu <dgkim723@hotmail.com>
2019-02-18 17:23:05 +0900

    Infomag-v2 Backend / 18.Feb.2019 - Method serializer for permission

--- UserAccess/serializers.py
+++ UserAccess/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import UserAccess
+
+
 # from Users.models import User
 # from Users.serializers import UserSerializer
 
@@ -7,6 +9,8 @@ from .models import UserAccess
 class UserAccessSerializer(serializers.ModelSerializer):
     account = serializers.SerializerMethodField(required=False, default='비어있음')
     user_name = serializers.SerializerMethodField(required=False, default='비어있음')
+    user_super = serializers.SerializerMethodField(required=False, default=False)
+    user_staff = serializers.SerializerMethodField(required=False, default=False)
     organization_name = serializers.SerializerMethodField(required=False, default='비어있음')
     company_name = serializers.SerializerMethodField(required=False, default='비어있음')
     phone = serializers.SerializerMethodField(required=False, default='비어있음')
@@ -24,6 +28,18 @@ class UserAccessSerializer(serializers.ModelSerializer):
             return str(obj.user.full_name)
         else:
             return '비어있음'
+
+    def get_user_staff(self, obj):
+        if obj.user is not None:
+            return obj.user.is_staff
+        else:
+            return False
+
+    def get_user_super(self, obj):
+        if obj.user is not None:
+            return obj.user.is_superuser
+        else:
+            return False
 
     def get_phone(self, obj):
         if obj.user.phone is not None:
@@ -59,7 +75,7 @@ class UserAccessSerializer(serializers.ModelSerializer):
         model = UserAccess
         fields = ('user', 'access', 'organization', 'company',
                   'account', 'user_name', 'organization_name', 'company_name', 'phone', 'email', 'company_org',
-                  'created_date', 'updated_date')
+                  'user_staff', 'user_super', 'created_date', 'updated_date')
         # fields = ('user', 'access', 'organization', 'company',
         #           'created_date', 'updated_date')
 
